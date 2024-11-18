deae0ff6a5876e2ea24a27411bf4618f12fbbe4e
Pavan <pavannaik0203@gmail.com>
2024-11-18 12:19:19 +0530

    backend functionality- compliance & Document

--- src/components/Collections/view/Documents.tsx
+++ src/components/Collections/view/Documents.tsx
@@ -6,18 +6,19 @@ import DocumentChecklist from "./Documentstabs/DocumentsChecklist";
 import Patents from "./Documentstabs/Patents";
 import Incubation from "./Documentstabs/Incubation";
 
+interface DocumentsProps {
+  startupId: string;
+  activeTab: string;
+}
 
-const Documents: React.FC = () => {
-  const [activeTab, setActiveTab] = useState("Document Checklist");
+const Documents: React.FC<DocumentsProps> = ({ activeTab, startupId }) => {
 
-  const tabs = ["Document Checklist", "Patents", "Incubation"];
-
-  const renderActiveSection = () => {
+  const renderTabContent = () => {
     switch (activeTab) {
       case "Document Checklist":
         return <DocumentChecklist />;
       case "Patents":
-        return <Patents />;
+        return <Patents startupId={startupId} />;
       case "Incubation":
         return <Incubation />;
       default:
@@ -30,7 +31,7 @@ const Documents: React.FC = () => {
       {/* Main Content */}
       <div className="bg-white p-4 rounded-lg shadow-md flex-grow">
         <h2 className="text-xl font-bold mb-4">{activeTab}</h2>
-        {renderActiveSection()}
+        {renderTabContent()}
       </div>
 
       
