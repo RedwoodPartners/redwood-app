767634a91774f84d69304e9c440e38eeac58e190
Pavan <pavannaik0203@gmail.com>
2025-01-12 00:17:29 +0530

    added Portfolio Section in Dashboard

--- src/app/(pages)/(protected)/home/page.tsx
+++ src/app/(pages)/(protected)/home/page.tsx
@@ -5,31 +5,59 @@ import { NoUsers } from "@/components/charts/noUsers";
 import { NoStartups } from "@/components/charts/noStartups";
 import StartupStats from "@/components/charts/startupStats";
 import { Domain } from "@/components/charts/domains";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const HomePage = () => {
+import { LineChartPortfolio } from "@/components/charts/lineChart";
+import { AreaChartPortfolio } from "@/components/charts/areaChart";
+import { BarChartPortfolio } from "@/components/charts/barChart";
+
+
+const Dashboard = () => {
   return (
     <>
-      <div className="">
-        {/* Startup Statistics Section */}
-        <div className="">
-          <StartupStats />
-        </div>
+      <Tabs defaultValue="dashboard" className="">
+        {/* Tabs List */}
+        <TabsList className="grid w-[200px] grid-cols-2">
+          <TabsTrigger value="dashboard" className="">
+            Dashboard
+          </TabsTrigger>
+          <TabsTrigger value="portfolio" className="">
+            Portfolio
+          </TabsTrigger>
+        </TabsList>
 
-        {/* Responsive Grid Section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-2 mx-auto">
-          <div className="">
-            <NoUsers />
+        {/* Tabs Content */}
+        <TabsContent value="dashboard">
+          {/* Startup Statistics Section */}
+          <div>
+            <StartupStats />
           </div>
-          <div className="">
-            <NoStartups />
+
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-2 mx-auto">
+            <div>
+              <NoUsers />
+            </div>
+            <div>
+              <NoStartups />
+            </div>
+            <div>
+              <Domain />
+            </div>
           </div>
-          <div className="">
-            <Domain />
-          </div>
+        </TabsContent>
+
+        <TabsContent value="portfolio">
+        <StartupStats />
+        <div className="grid grid-cols-3 gap-4 p-2">
+          <LineChartPortfolio />
+          <AreaChartPortfolio />
+          <BarChartPortfolio />
         </div>
-      </div>
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
 
-export default HomePage;
+export default Dashboard;
